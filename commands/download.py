01f942fb4490b70c774e8779944095fb6474113a
amtp1 <magomedov.abdul20012@gmail.com>
2022-01-20 23:28:26 +0300

    show video size

--- commands/download.py
+++ commands/download.py
@@ -12,9 +12,12 @@ from models.models import *
 @dp.message_handler()
 async def download(message: Message):
     try:
-        await message.answer(text="Downloading...")
         yt = YouTube(message.text)
         stream = yt.streams.get_by_itag(22)
+        mb_size = int(stream.filesize / 8 / 8 / 16 / 1024)
+        if mb_size > 50:
+            return await message.answer(text=f"Video size ({mb_size}MB) large then 50MB")
+        await message.answer(text=f"⏳Downloading best video ({mb_size}MB)")
         video = urllib.request.urlopen(stream.url).read()
         bytes_video = BytesIO(video)
         user = await User.objects.get(user_id=message.from_user.id)
@@ -22,9 +25,10 @@ async def download(message: Message):
         await user.update(download_count=download_count)
         return await bot.send_video(
             message.from_user.id, InputFile(bytes_video, filename=f"{yt.author} - {yt.title}"),
-            caption=f"{yt.author} - {yt.title}\n\n"
+            caption=f"✅ <b>{yt.author}</b> - {yt.title}\n\n"
             f"Channel: @downloader_video"
         )
+
     except RegexMatchError:
         return await message.answer(text="Invalid link!")
     except Exception as e:
