7e4ecb243d11a011a608f3a8fca60d98b7b08661
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-01 00:20:13 +0300

    fixed bug

--- commands/statistics.py
+++ commands/statistics.py
@@ -8,7 +8,7 @@ from models.models import *
 async def statistics(message: Message):
     if int(config.get("ADMIN_ID")) == message.from_user.id:
         users = await User.objects.all()
-        download_count = await User.objects.count()
+        download_count = sum(list(obj.download_count for obj in users))
         stat_page: str = (f"<b>Statistics</b>\n"
             f"<code>|--</code><i>All users</i>: {users.__len__()}\n"
             f"<code>|--</code><i>Download count</i>: {download_count}\n"
