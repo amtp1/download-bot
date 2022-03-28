2b5024a7e04e88c80c450a6eaf7c3c04aa74d2d3
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-28 22:29:45 +0300

    add update download count function

--- objects/globals.py
+++ objects/globals.py
@@ -1,4 +1,11 @@
+from models.mongo.models import *
+
 metadata = None
 db_engine = None
 
 config: dict = {}
+
+def update_download_count(user_id: int):
+    user = User(user_id=user_id)
+    user.download_count = user.download_count + 1
+    user.save()
