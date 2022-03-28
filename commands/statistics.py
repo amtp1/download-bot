ceed903f6dce53c5b41ce6bfd47992d489ad04ae
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-28 22:28:56 +0300

    refactor

--- commands/statistics.py
+++ commands/statistics.py
@@ -1,13 +1,13 @@
 from aiogram.types import Message
 
 from objects.globals import dp, config
-from models.models import *
+from models.mongo.models import *
 
 
 @dp.message_handler(commands=["stat"])
 async def statistics(message: Message):
     if int(config.get("ADMIN_ID")) == message.from_user.id:
-        users = await User.objects.all()
+        users = User.objects.all()
         download_count = sum(list(obj.download_count for obj in users))
         stat_page: str = (f"<b>Statistics</b>\n"
             f"<code>|--</code><i>All users</i>: {users.__len__()}\n"
