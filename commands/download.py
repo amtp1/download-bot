815dc9a788ef2b7581a5315d4da631c9f7e0186b
amtp1 <magomedov.abdul20012@gmail.com>
2022-04-03 23:07:54 +0300

    refactoring

--- commands/download.py
+++ commands/download.py
@@ -4,15 +4,34 @@ from io import BytesIO
 import yarl
 from loguru import logger
 from pytube import YouTube
+from dataclasses import dataclass
 from pytube.exceptions import RegexMatchError
 from aiogram.dispatcher.storage import FSMContext
-from aiogram.types import Message, InputFile, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, InputFile
 
 from objects.globals import dp, bot, update_download_count
 from models.mongo.models import *
 from states.states import *
 
 SCHEME = {"https", "http"} # Scheme types.
+
+
+@dataclass
+class MetaDownload:
+    is_error: bool = False
+    message: str = ""
+    stream: str = ""
+
+    author: str = ""
+    title: str = ""
+
+    def __init__(self, is_error=False, message="", stream="", author="", title=""):
+        self.is_error = is_error
+        self.message = message
+        self.stream = stream
+        self.author = author
+        self.title = title
+
 
 @dp.message_handler()
 async def download(message: Message, state: FSMContext):
@@ -36,42 +55,63 @@ async def download(message: Message, state: FSMContext):
 
 @dp.callback_query_handler(lambda query: query.data=="audio")
 async def download_audio(query: CallbackQuery, state: FSMContext):
+    cht_id = query.from_user.id
+    msg_id = query.message.message_id
+
     content_data: dict = await state.get_data() # Get state data.
-    yt: YouTube = YouTube(content_data.get("url")) # Init YouTube class.
-    stream = yt.streams.filter(only_audio=True)[0]
-    mb_size: float = float(stream.filesize / 8 / 8 / 16 / 1024) # Convert to mb value size.
-    str_filesize: str = "{:.2}".format(mb_size) # Format in string file size.
-    if mb_size > 50:
-        return await bot.send_message(chat_id=query.from_user.id, text=f"Audio size ({str_filesize})MB) large then 50MB") # File size is larger.
-    await bot.send_message(chat_id=query.from_user.id, text=f"⏳Downloading best audio ({str_filesize}MB)")
-    audio = urllib.request.urlopen(stream.url).read() # Read audio content.
-    bytes_audio: BytesIO = BytesIO(audio) # Convert audio content in bytes.
+    response = yt_download(content_data.get("url"), is_audio=True)
+    
+    if response.is_error:
+        return await bot.send_message(chat_id=cht_id, text=response.message)
+    else:
+        await bot.edit_message_text(chat_id=cht_id, message_id=msg_id, text=f"⏳Downloading best audio")
+        audio = urllib.request.urlopen(response.stream).read() # Read audio content.
+        bytes_audio: BytesIO = BytesIO(audio) # Convert audio content in bytes.
 
-    # Update user download count.
-    update_download_count(query.from_user.id)
+        # Update user download count.
+        update_download_count(query.from_user.id)
 
-    return await bot.send_audio(
-        query.from_user.id, InputFile(bytes_audio, filename=f"{yt.author} - {yt.title}"),
-        caption=f"✅ <b>{yt.author}</b> - {yt.title}\n\n"
-        f"Channel: @downloader_video") # Return audio with description.
+        return await bot.send_audio(
+            query.from_user.id, InputFile(bytes_audio, filename=f"{response.author} - {response.title}"),
+            caption=f"✅ <b>{response.author}</b> - {response.title}\n\n"
+            f"Channel: @downloader_video") # Return audio with description.
 
 @dp.callback_query_handler(lambda query: query.data=="video")
 async def download_video(query: CallbackQuery, state:FSMContext):
+    cht_id = query.from_user.id
+    msg_id = query.message.message_id
+
     content_data = await state.get_data() # Get state data.
-    yt: YouTube = YouTube(content_data.get("url")) # Init YouTube class.
-    stream = yt.streams.filter(only_video=True)[0]
+    response = yt_download(content_data.get("url"))
+
+    if response.is_error:
+        return await bot.edit_message_text(chat_id=cht_id, message_id=msg_id, text=response.message)
+    else:
+        await bot.edit_message_text(chat_id=cht_id, message_id=msg_id, text=f"⏳Downloading best video")
+        video = urllib.request.urlopen(response.stream).read() # Read video content.
+        bytes_video: BytesIO = BytesIO(video) # Convert video content in bytes.
+
+        # Update user download count.
+        update_download_count(query.from_user.id)
+
+        return await bot.send_video(
+            query.from_user.id, InputFile(bytes_video, filename=f"{response.author} - {response.title}"),
+            caption=f"✅ <b>{response.author}</b> - {response.title}\n\n"
+            f"Channel: @downloader_video") # Return video with description.
+
+def yt_download(url: str, is_audio: bool = False) -> dict:
+    yt: YouTube = YouTube(url) # Init YouTube class.
+    if is_audio:
+        stream = yt.streams.filter(only_audio=is_audio)[0]
+    else:
+        stream = yt.streams.filter(only_video=True)[0]
     mb_size: float = float(stream.filesize / 8 / 8 / 16 / 1024) # Convert to mb value size.
-    str_filesize: str = "{:.2}".format(mb_size) # Format in string file size.
+    if mb_size < 1024:
+        str_filesize: str = "{:.2}MB".format(mb_size) # Format in string file size.
+    elif mb_size > 1024:
+        _mb_size = mb_size / 1024
+        str_filesize: str = "{:.2}GB".format(_mb_size)
     if mb_size > 50:
-        return await bot.send_message(chat_id=query.from_user.id, text=f"Video size ({str_filesize})MB) large then 50MB") # File size is larger.
-    await bot.send_message(chat_id=query.from_user.id, text=f"⏳Downloading best video ({str_filesize}MB)")
-    video = urllib.request.urlopen(stream.url).read() # Read video content.
-    bytes_video: BytesIO = BytesIO(video) # Convert video content in bytes.
-
-    # Update user download count.
-    update_download_count(query.from_user.id)
-
-    return await bot.send_video(
-        query.from_user.id, InputFile(bytes_video, filename=f"{yt.author} - {yt.title}"),
-        caption=f"✅ <b>{yt.author}</b> - {yt.title}\n\n"
-        f"Channel: @downloader_video") # Return video with description.
+        return MetaDownload(is_error=True, message=f"Video size ({str_filesize}) large then 50MB")
+    else:
+        return MetaDownload(stream=stream.url, author=yt.author, title=yt.title)
