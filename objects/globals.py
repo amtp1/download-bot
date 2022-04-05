4daee07da6cc8ef159e8ea3666ddc4b054878f8e
amtp1 <magomedov.abdul20012@gmail.com>
2022-04-05 15:57:25 +0300

    refactoring

--- objects/globals.py
+++ objects/globals.py
@@ -8,4 +8,4 @@ config: dict = {}
 def update_download_count(user_id: int):
     user = User.objects(user_id=user_id).first()
     update_fields = {"download_count": user.download_count+1}
-    res = User.objects.update_by_user_id(user.user_id, update_fields)
+    User.objects.update_by_user_id(user.user_id, update_fields)
