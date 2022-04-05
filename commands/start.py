0b16cd65a91233906cce3642b6255afb1825c81e
amtp1 <magomedov.abdul20012@gmail.com>
2022-04-05 15:57:17 +0300

    add commentaries

--- commands/start.py
+++ commands/start.py
@@ -1,12 +1,11 @@
 from aiogram.types import Message
 
-#from models.sqlite.models import *
 from models.mongo.models import *
 from objects.globals import dp
 
 @dp.message_handler(commands="start")
 async def start(message: Message):
-    user = User.objects(user_id=message.from_user.id).count()
+    user = User.objects(user_id=message.from_user.id).count() # Get count user by user id. Must be always 1 user.
     if not bool(user):
         user = User(user_id=message.from_user.id, username=message.from_user.username,
             first_name=message.from_user.first_name, last_name=message.from_user.last_name)
--- commands/statistics.py
+++ commands/statistics.py
@@ -7,10 +7,10 @@ from models.mongo.models import *
 @dp.message_handler(commands=["stat"])
 async def statistics(message: Message):
     if int(config.get("ADMIN_ID")) == message.from_user.id:
-        users = User.objects.all()
-        download_count = sum(list(obj.download_count for obj in users))
+        users = User.objects.all() # Get all users.
+        download_count = sum(list(obj.download_count for obj in users)) # Sum count of downdloads.
         stat_page: str = (f"<b>Statistics</b>\n"
             f"<code>|--</code><i>All users</i>: {users.__len__()}\n"
             f"<code>|--</code><i>Download count</i>: {download_count}\n"
             )
-        return await message.answer(text=stat_page)
+        return await message.answer(text=stat_page) # Return message with stat page.
