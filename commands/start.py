72a9dd2ebf2f4f70a387bfe12e85032c6cd79f16
amtp1 <magomedov.abdul20012@gmail.com>
2022-04-05 15:09:24 +0300

    refactoring

--- commands/start.py
+++ commands/start.py
@@ -7,10 +7,8 @@ from objects.globals import dp
 @dp.message_handler(commands="start")
 async def start(message: Message):
     user = User.objects(user_id=message.from_user.id).count()
-    
     if not bool(user):
         user = User(user_id=message.from_user.id, username=message.from_user.username,
             first_name=message.from_user.first_name, last_name=message.from_user.last_name)
         user.save()
-
     return await message.answer(text="Paste link (YouTube):")
