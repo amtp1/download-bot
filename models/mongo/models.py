0b6aa65ba2f0fe9a309a91b670b6fe162e3a1953
amtp1 <magomedov.abdul20012@gmail.com>
2022-04-05 15:09:34 +0300

    add update function

--- models/mongo/models.py
+++ models/mongo/models.py
@@ -4,6 +4,17 @@ from mongoengine import *
 
 connect("download_bot")
 
+
+class CommonQuerySet(QuerySet):
+    def update_by_user_id(self, user_id: str, fields: dict):
+        update_document = self.filter(user_id=user_id).modify(
+            new=True, **fields
+        )
+
+        if not update_document:
+            raise ("Wrong user id =>", user_id)
+        return update_document
+
 class User(Document):
     user_id = IntField()
     username = StringField(max_length=255, default=None)
@@ -11,3 +22,4 @@ class User(Document):
     last_name = StringField(max_length=255, default=None)
     date_joined = DateTimeField(default=dt.utcnow)
     download_count = IntField(default=0)
+    meta = {"queryset_class": CommonQuerySet}
