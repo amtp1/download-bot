3f16675d7726800c5af3c3e2011f6157fa879969
anonymous <info@1inch.io>
2022-04-25 00:21:04 +0300

    web app

---
+++ api/app.py
@@ -0,0 +1,37 @@
+from uuid import uuid4
+
+from flask import Flask, jsonify
+
+from models.mongo.models import *
+from models.mongo.models import Admins
+from models.mongo.models import DesktopSession
+
+#from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    users = User.objects.all()
+    serialize_users = [user.serialize() for user in users]
+    return jsonify({"users": serialize_users})
+
+@app.route('/api/v1/auth/<email>/<password>', methods=['GET'])
+def auth(email, password):
+    admin = Admins.objects.filter(email=email, password=password)
+
+    if not admin:
+        return jsonify({"token": ""})
+    else:
+        desktop_session = DesktopSession.objects.filter(user_email=email)
+        if not desktop_session:
+            DesktopSession.objects.create(user_email=email)
+        else:
+            generate_token: str = str(uuid4())
+            desktop_session = desktop_session.first()
+            update_fields = {"session_id": generate_token}
+            DesktopSession.objects.update(email, update_fields)
+            return jsonify({"token": generate_token})
+
+if __name__ == "__main__":
+    app.run(debug=True)
