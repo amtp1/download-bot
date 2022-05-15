fb9aabf4cc0e2c776e3dd63987a6dc661a5fa62c
anonymous <info@1inch.io>
2022-05-15 16:56:43 +0300

    add update blocked user status function

--- objects/globals.py
+++ objects/globals.py
@@ -13,3 +13,8 @@ def update_download_count(user_id: int):
     user = User.objects(user_id=user_id).first()
     update_fields = {"download_count": user.download_count+1}
     User.objects.update(user.user_id, update_fields)
+
+def update_blocked_status(user_id: int):
+    user = User.objects(user_id=user_id).first()
+    update_fields = {"is_blocked": True}
+    User.objects.update(user.user_id, update_fields)
