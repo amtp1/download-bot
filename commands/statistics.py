06e35bdc43e83a51789753188e80b26f228c9f2e
amtp1 <magomedov.abdul20012@gmail.com>
2022-11-06 00:06:28 +0300

    refactoring

--- commands/statistics.py
+++ commands/statistics.py
@@ -8,6 +8,7 @@ from models.mongo.models import *
 async def statistics(message: Message):
     if int(config.get("ADMIN_ID")) == message.from_user.id:
         users = User.objects.all()  # Get all users.
+        blocked_users = User.objects.filter(is_blocked=True).all()
         # Sum count of downdloads.
         download_count = sum(list(obj.download_count for obj in users))
         mail_markup = ReplyKeyboardMarkup(resize_keyboard=True,
@@ -15,9 +16,12 @@ async def statistics(message: Message):
                                               [KeyboardButton(
                                                   text="Conduct mailing")]
                                           ])
-        stat_page: str = (f"<b>Statistics</b>\n"
-                          f"<code>|--</code><i>All users</i>: {users.__len__()}\n"
-                          f"<code>|--</code><i>Download count</i>: {download_count}\n"
+        stat_page: str = (f"<b>Statistics</b>\n\n"
+                          f"👤\n"
+                          f"<code>|__</code> <i>All users</i>: {users.__len__()}\n"
+                          f"<code>   |__</code> <i>Blocked users count</i>: {blocked_users.count()}\n"
+                          f"💾\n"
+                          f"<code>|__</code> <i>Download count</i>: {download_count}\n"
                           )
         # Return message with stat page.
         return await message.answer(text=stat_page, reply_markup=mail_markup)
