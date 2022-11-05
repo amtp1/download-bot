5250bd1453b1e4118dd12d3f98b533c36bca7326
amtp1 <magomedov.abdul20012@gmail.com>
2022-11-05 22:18:36 +0300

    refactoring

--- commands/help.py
+++ commands/help.py
@@ -6,6 +6,6 @@ from objects.globals import dp
 async def help(message: Message):
     help_page = ("1) Bot is designed to upload video and audio from YouTube.\n"
                  "2) Bot will not be able to send a file that weighs more than 50mb.\n"
-                 "3) To upload video or audio from YouTube, just paste the link.\n"
+                 "3) To upload video or audio from YouTube, just paste the link.\n\n"
                  "Help chat: @AnyswapNetwork")
     return await message.answer(text=help_page)
