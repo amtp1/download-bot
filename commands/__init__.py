f74be2f9ac75513c8e674ef42e8bc53e29f70165
amtp1 <magomedov.abdul20012@gmail.com>
2023-01-18 11:25:54 +0300

    unset download

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,8 +3,6 @@ from os.path import dirname, basename, isfile, join
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(m)[:-3] for m in modules if isfile(m)
-           and not m.endswith("__init__.py")
-           and not m.endswith("download.py")]
+           and not m.endswith("__init__.py")]
 
 from . import *
-from . import download
