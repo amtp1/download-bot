9c0fda1d452d393293871d19b2405c7b8546e279
amtp1 <magomedov.abdul20012@gmail.com>
2023-01-18 11:27:51 +0300

    move markup in keyboards module

--- commands/start.py
+++ commands/start.py
@@ -2,6 +2,7 @@ from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 from models.mongo.models import *
 from objects.globals import dp, config
+from keyboards.keyboards import start_markup
 
 
 @dp.message_handler(commands="start")
@@ -12,10 +13,6 @@ async def start(message: Message):
         user = User(user_id=message.from_user.id, username=message.from_user.username,
                     first_name=message.from_user.first_name, last_name=message.from_user.last_name)
         user.save()
-    reply_markup = ReplyKeyboardMarkup(resize_keyboard=True,
-                                       keyboard=[
-                                           [KeyboardButton(text="❗️Help")]
-                                       ])
     if config.get("ADMIN_ID") == message.from_user.id:
-        reply_markup.add(KeyboardButton("Conduct mailing"))
-    return await message.answer(text="Paste link (YouTube):", reply_markup=reply_markup)
+        start_markup.add(KeyboardButton("Conduct mailing"))
+    return await message.answer(text="Paste link (YouTube):", reply_markup=start_markup)
