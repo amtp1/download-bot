574881ba60b2764937a814dc96cebe723a3fe611
amtp1 <magomedov.abdul20012@gmail.com>
2023-01-18 22:24:27 +0300

    refactoring

--- commands/instagram.py
+++ commands/instagram.py
@@ -1,17 +1,38 @@
+import re
+import urllib.request
+from io import BytesIO
+
+import requests
 from aiogram.types import Message
 from aiogram.dispatcher.storage import FSMContext
 
-from objects.globals import dp
+from objects.globals import dp, bot
 from utils.downloader.instagram import InstagramDownloader
 
 @dp.message_handler(lambda message: message.text == 'Instagram Stories', state='*')
 async def download(message: Message, state: FSMContext):
-    #  await message.answer("Input username (Without @)👇")
-    #  return await state.set_state('get_username')
-    return await message.answer("Soon to the moon! In processing ...")
+    await message.answer("Input username (Without @)👇")
+    return await state.set_state('get_username')
+    # return await message.answer("Soon to the moon! In processing ...")
 
 
 @dp.message_handler(state='get_username')
-async def get_username(message: Message):
+async def get_username(message: Message, n=0):
     username = message.text
-    stories = InstagramDownloader(username).stories()
+    instagram_downloader = InstagramDownloader(username)
+    stories = instagram_downloader.stories()
+    proxies = instagram_downloader.get_proxies()
+    for story in stories:
+        n+=1
+        if n % 2 == 0:
+            content_id, content_type, url = story.get('id'), story.get('type'), story.get('url')
+            f_url = re.sub('/api/proxy/', '', url)
+            if content_type == "photo":
+                #photo = requests.get(url=f_url, stream=True).content
+                proxy_handler = urllib.request.ProxyHandler(proxies)
+                opener = urllib.request.build_opener(proxy_handler)
+                opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+                urllib.request.install_opener(opener)
+                photo = urllib.request.urlopen(f_url).read()
+                b_photo: BytesIO = BytesIO(photo)
+                await bot.send_photo(message.from_user.id, photo=b_photo, caption=f"Photo ID: {content_id}")
--- utils/downloader/instagram.py
+++ utils/downloader/instagram.py
@@ -1,4 +1,4 @@
-import re
+import json
 import random
 from dataclasses import dataclass
 
@@ -11,7 +11,7 @@ config = dict(dotenv_values('.proxy_env'))
 PROXIES = list(dotenv_values('.proxies'))
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
-MULTI_PROXY = True
+MULTI_PROXY = False
 
 @dataclass
 class ContentMeta:
@@ -29,32 +29,12 @@ class InstagramDownloader:
         self.username = username
 
     def stories(self):
-        try:
-            user_id = self.get_user_id()
-            if user_id:
-                proxies = self.get_proxies()
-                link = 'https://igs.sf-converter.com/api/stories/%s' % user_id
-                request = requests.get(url=link, proxies=proxies, timeout=3)
-                response = request.text
-                return response
-        except (ProxyError, ConnectTimeout, SSLError):
-            self.stories()
-
-    def get_user_id(self) -> str:
-        try:
-            link = 'https://www.instagram.com/%s/?__a=1' % self.username
-            request = requests.get(url=link, proxies=self.get_proxies(), timeout=3)
-            response = request.text
-            if response:
-                profile_id = re.search('"profile_id"', response)
-                sub_path = re.search('"sub_path"', response)
-                profile_id = response[profile_id.end():sub_path.start()]
-                profile_id = re.sub('[:",]', '', profile_id)
-                return profile_id
-        except ConnectionError:
-            self.get_user_id()
-        except HTTPError as e:
-            logger.error(e)
+        link = 'https://instastories.watch/api/profile/stories?username=%s' % self.username
+        request = requests.get(url=link)
+        response = request.text
+        if response:
+            response = json.loads(response)
+        return response
 
     def get_proxies(self):
         if MULTI_PROXY:
