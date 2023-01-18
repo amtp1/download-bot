88eebff55cc55de9933e90567932b5c2f32df77b
amtp1 <magomedov.abdul20012@gmail.com>
2023-01-18 11:30:15 +0300

    add video_id State field in Type StatesGroup

--- states/states.py
+++ states/states.py
@@ -1,10 +1,8 @@
-
 from aiogram.dispatcher.filters.state import StatesGroup, State
-
 
 class Type(StatesGroup):
     url = State()
-
+    video_id = State()
 
 class Mailing(StatesGroup):
     condunt_mailing = State()
