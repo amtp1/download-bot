d3d1731d0d9fefcef885d1d641ac72cc9d3cfc57
amtp1 <magomedov.abdul20012@gmail.com>
2023-01-19 12:10:20 +0300

    add downloads

--- api/app.py
+++ api/app.py
@@ -11,25 +11,30 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html", content="Test")
+    return render_template("index.html", content='Test')
 
 @app.route('/users', methods=['GET'])
 def users():
     users = User.objects.all()
-    return render_template("users.html", users=users)
+    return render_template('users.html', users=users)
+
+@app.route('/downloads', methods=['GET'])
+def downloads():
+    downloads = [download.serialize() for download in Download.objects.all()]
+    return render_template('downloads.html', downloads=downloads)
 
 @app.route('/api/v1/users', methods=['GET'])
 def get_users():
     users = User.objects.all()
     serialize_users = [user.serialize() for user in users]
-    return jsonify({"users": serialize_users})
+    return jsonify({'users': serialize_users})
 
 @app.route('/api/v1/auth/<email>/<password>', methods=['GET'])
 def auth(email, password):
     admin = Admins.objects.filter(email=email, password=password)
 
     if not admin:
-        return jsonify({"token": ""})
+        return jsonify({'token': ''})
     else:
         desktop_session = DesktopSession.objects.filter(user_email=email)
         if not desktop_session:
@@ -39,7 +44,7 @@ def auth(email, password):
             desktop_session = desktop_session.first()
             update_fields = {"session_id": generate_token}
             DesktopSession.objects.update(email, update_fields)
-            return jsonify({"token": generate_token})
+            return jsonify({'token': generate_token})
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
