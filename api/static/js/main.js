844769eee67d97e0ef856850d078aca22377efa8
amtp1 <magomedov.abdul20012@gmail.com>
2023-01-19 12:11:21 +0300

    add download search handler

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -1,10 +1,10 @@
 function searchUserById() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("SearchUserInput");
+    input = document.getElementById('SearchUserInput');
     filter = input.value.toUpperCase();
-    table = document.getElementById("UsersTable");
-    tr = table.getElementsByTagName("tr");
+    table = document.getElementById('UsersTable');
+    tr = table.getElementsByTagName('tr');
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
@@ -14,12 +14,38 @@ function searchUserById() {
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
         } else {
-          tr[i].style.display = "none";
+          tr[i].style.display = 'none';
         }
       }
     }
 }
 
+function searchDownloadByUserId() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById('SearchDownloadInput');
+  filter = input.value.toUpperCase();
+
+  lg = $('#downloadsUl li')
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < lg.length; i++) {
+    li = lg[i];
+    if (li){
+      txtValue = li.textContent || li.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        lg[i].style.display = "";
+      } else {
+        lg[i].style.display = 'none';
+      }
+    }
+  }
+}
+
 function redirect_to_users(){
-    window.location.replace("/users");
+    window.location.replace('/users');
+}
+
+function redirect_to_downloads(){
+  window.location.replace('/downloads');
 }
